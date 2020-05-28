32abdafd38ced389152ac74a9826270fe1a8b904
owpk <vzvz4@mail.ru>
2020-05-28 19:07:34 +0900

    hw_2

---
+++ src/main/java/filter/MyFilter.java
@@ -0,0 +1,35 @@
+package filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+@WebFilter(urlPatterns = "/*")
+public class MyFilter implements Filter {
+
+    private transient FilterConfig filterConfig;
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain filter) throws IOException, ServletException {
+        HttpServletRequest httpReq = (HttpServletRequest) req;
+        HttpServletResponse httpResp = (HttpServletResponse) resp;
+        httpResp.addHeader("X-CharSet", "UTF-8");
+        httpResp.setContentType("text/html; charset=utf-8");
+        httpResp.getWriter().write("<header><a href=\"/main\">| main |</a>");
+        httpResp.getWriter().write("<a href=\"/cart\">| cart |</a>");
+        httpResp.getWriter().write("<a href=\"/order\">| order |</a>");
+        httpResp.getWriter().write("<a href=\"/catalog\">| catalog |</a>");
+        httpResp.getWriter().write("<a href=\"/product\">| product |</a></header>");
+        filter.doFilter(httpReq, httpResp);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        this.filterConfig = filterConfig;
+    }
+}
