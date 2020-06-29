e2ad331f702fe21dc2b68dcd68ca6f3b8682c40c
owpk <vzvz4@mail.ru>
2020-06-29 22:18:19 +0900

    fix

--- src/main/java/lesson6/controller/CartController.java
+++ src/main/java/lesson6/controller/CartController.java
@@ -1,4 +1,27 @@
 package lesson6.controller;
 
-public class CartController {
+import lesson6.service.CartService;
+import lesson6.service.ProdRepresentation;
+
+import javax.ejb.EJB;
+import javax.enterprise.context.SessionScoped;
+import javax.inject.Named;
+import java.io.Serializable;
+import java.util.List;
+
+@SessionScoped
+@Named
+public class CartController implements Serializable {
+
+  @EJB
+  private CartService cartService;
+
+  public void add(ProdRepresentation prod) {
+    cartService.add(prod);
+  }
+
+  public List<ProdRepresentation> getAllUserProd() {
+    return cartService.getCart();
+  }
+
 }
